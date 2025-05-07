7655da360f877d9603ae76f8bbc6c904cafcd34e
Emiliano2122 <103003739+Emiliano2122@users.noreply.github.com>
2025-05-06 22:01:15 -0600

    actualizacion del contenido del CursoDePython porque el tema 00_hellow.py solo era para seber como iniciar con este curso de Python

--- 01_variables.py
+++ 01_variables.py
@@ -13,4 +13,36 @@ print(type(my_int_to_str_variables))
 my_bool_variable = False
 print(my_bool_variable)
 
+# Concatenación de variables en un print
 print(my_string_variable, my_int_to_str_variables, my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
+
+# Algunas funciones del sistema
+print(len(my_string_variable))
+
+# Variables en una sola linea. ¡Cuidado con abusar de esta sintaxis!
+name, surname, alias, age = "Emiliano", "Stasi", 'emilianostasi', 22
+print("Me llamo:", name, surname, ". Mi edad es:", age, ". Y mi alias es:", alias)
+
+# Input
+"""
+name = input('¿Cual es tu nombre?')
+age = input('¿Cuantos años tienes?')
+print(name)
+print(age)
+"""
+
+# Cambiamos su tipo
+name = 22
+age = "Emiliano"
+print(name)
+print(age)
+
+
+
+# ¿Forzamos el tipo?
+address: str = "Mi dirección"
+address = True
+address = 5
+address = 1.2
+print(type(address))
---
+++ 02_operators.py
@@ -0,0 +1,32 @@
+### Operadores Aritmeticos ###
+
+# Operadores con enteros
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 -7 / 1 // 4)
+
+# Operdadores con cadenas de texto
+print("Hola " + "Python " + "¿Que tal?")
+print("Hola " + str(5))
+
+# Operaciones mixtas
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores con enteros ###
+
+#Operadores con enteris
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
